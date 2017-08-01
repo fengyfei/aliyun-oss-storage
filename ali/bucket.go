7ceda6db6432f39c8e9867ec08215179825578cb
xiaoziqingren <1045977704@qq.com>
2017-08-01 20:32:36 +0800

    设置 bucket 权限 #8
    设置 bucket 权限

---
+++ ali/bucket.go
@@ -0,0 +1,51 @@
+/*
+* MIT License
+*
+* Copyright (c) 2017 SmartestEE Co,Ltd..
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy of
+* this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+ */
+
+/*
+* Revision History
+*     Initial: 2017-08-01          Sun Anxiang
+ */
+
+package ali
+
+import (
+	"github.com/aliyun/aliyun-oss-go-sdk/oss"
+
+	"aliyun-oss-storage/config"
+	"aliyun-oss-storage/log"
+)
+
+type OssClient struct {
+	Client	*oss.Client
+}
+
+// SetBucketACL is to set the authority of bucket with the config bucketName.
+func (this *OssClient) SetBucketACL(bucketACL oss.ACLType) error {
+	err := this.Client.SetBucketACL(config.Conf.BucketName, bucketACL)
+	if err != nil {
+		log.Logger.Error("[ERROR]", err)
+	}
+
+	return err
+}
